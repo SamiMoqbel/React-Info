2e579a70634f956602ee0673622905d8a44ef081
Sami Moqbel <moqbelsami@gmail.com>
2023-10-14 13:56:13 +0300

    edits

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,16 @@ import '../styles/Header.css'
 
 export default function Header(){
     return(
-      <div className="navbar">
+      <div className="dark--navbar">
   
         <nav>
           <div className="header-container">
             <img className="logo-pic" src={reactLogo} />
             <h3 className="header-title">ReactFacts</h3>
           </div>
-          <h4 className="course-title">React Course - Project 1</h4>
+
+          
+          
         </nav>
   
       </div>
